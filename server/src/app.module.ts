990dbe77310e790d3ca80a3be18d9255fd3a3bc8
Grigory-js-ninja <grigory@maroo.us>
2022-07-17 17:37:57 +0300

    feat: implement gql server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,39 @@
+import { join as joinPath } from 'path';
+
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
+import { ConfigModule } from '@nestjs/config';
+
+import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+    }),
+    RecipesModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: joinPath(process.cwd(), 'src/schema.gql'),
+      transformSchema: (schema) => upperDirectiveTransformer(schema, 'upper'),
+      installSubscriptionHandlers: true,
+      buildSchemaOptions: {
+        directives: [
+          new GraphQLDirective({
+            name: 'upper',
+            locations: [DirectiveLocation.FIELD_DEFINITION],
+          }),
+        ],
+      },
+      definitions: {
+        path: joinPath(process.cwd(), 'src/graphql.schema.ts'),
+        // outputAs: 'class',
+      },
+    }),
+  ],
 })
 export class AppModule {}
