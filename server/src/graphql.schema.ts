58f58e85ce1885e57d2f24db2bb126d5cea4704b
Grigory-js-ninja <grigory@maroo.us>
2022-07-18 21:33:12 +0300

    feat: add user, passport, address, phone, fingerprint schemas

--- server/src/graphql.schema.ts
+++ server/src/graphql.schema.ts
@@ -12,6 +12,51 @@ export class CreateCatInput {
     age?: Nullable<number>;
 }
 
+export class CreatePassportInput {
+    user: User;
+    identifier: string;
+    number: string;
+}
+
+export class UpdatePassportInput {
+    fingerprint: Fingerprint;
+}
+
+export class UpdatePhoneInput {
+    countryCode?: Nullable<number>;
+    number?: Nullable<number>;
+}
+
+export class UpdateAddressInput {
+    country?: Nullable<string>;
+    city?: Nullable<string>;
+    line1?: Nullable<string>;
+    line2?: Nullable<string>;
+    zip?: Nullable<number>;
+}
+
+export class CreateUserInput {
+    firstName: string;
+    lastName: string;
+    birthDate: string;
+    countryResident: string;
+}
+
+export class DeleteUserInput {
+    id: string;
+}
+
+export class UpdateUserInput {
+    id: string;
+    firstName?: Nullable<string>;
+    lastName?: Nullable<string>;
+    birthDate?: Nullable<string>;
+    countryResident?: Nullable<string>;
+    isVerified?: Nullable<boolean>;
+    address?: Nullable<UpdateAddressInput>;
+    phone?: Nullable<UpdatePhoneInput>;
+}
+
 export abstract class IQuery {
     abstract cats(): Nullable<Nullable<Cat>[]> | Promise<Nullable<Nullable<Cat>[]>>;
 
@@ -38,6 +83,48 @@ export class Cat {
     name?: Nullable<string>;
     age?: Nullable<number>;
     owner?: Nullable<Owner>;
+}
+
+export class Fingerprint {
+    id?: Nullable<string>;
+    publicKey: string;
+    privateKey: string;
+}
+
+export class Passport {
+    id: string;
+    identifier: string;
+    number: string;
+    user: User;
+    fingerprint: Fingerprint;
+}
+
+export class Phone {
+    id?: Nullable<string>;
+    countryCode?: Nullable<number>;
+    number?: Nullable<number>;
+}
+
+export class Address {
+    id?: Nullable<string>;
+    country?: Nullable<string>;
+    city?: Nullable<string>;
+    line1?: Nullable<string>;
+    line2?: Nullable<string>;
+    zip?: Nullable<number>;
+}
+
+export class User {
+    id?: Nullable<string>;
+    firstName: string;
+    lastName: string;
+    birthDate: string;
+    countryResident: string;
+    age?: Nullable<number>;
+    isVerified?: Nullable<boolean>;
+    passport?: Nullable<Passport>;
+    address?: Nullable<Address>;
+    phone?: Nullable<Phone>;
 }
 
 type Nullable<T> = T | null;
