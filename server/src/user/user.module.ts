5e25146d74f758df37955e720542333b12b531e6
Grigory-js-ninja <grigory@maroo.us>
2022-07-21 15:08:27 +0300

    fix: DI for user entity

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserResolver } from '~/user/user.resolver';
 import { UserService } from '~/user/user.service';
 import { UtilsModule } from '~/utils/utils.module';
+import { UserEntity } from '~/user/user.entity';
 @Module({
-  imports: [UtilsModule],
+  imports: [UtilsModule, TypeOrmModule.forFeature([UserEntity])],
   providers: [UserResolver, UserService],
   exports: [UserService],
 })
