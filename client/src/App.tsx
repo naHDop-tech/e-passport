efcc1d7c4152dfbb4edda374b19e47b16d1e7401
Grigory-js-ninja <grigory@maroo.us>
2022-07-22 14:36:44 +0300

    chore: delete stories folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import { Button } from './components/common/Buttons/Primary'
-import { ButtonColors } from './components/common/Buttons/Primary'
+import { ButtonTypes } from './components/common/Buttons/Primary'
 
 export function App(): JSX.Element {
   return (
     <div>
       <h1>E-Passport</h1>
-      <Button bType={ButtonColors.Danger} onClick={() => console.log('button pushed')}>Push my</Button>
+      <Button bType={ButtonTypes.Danger} onClick={() => console.log('button pushed')}>Push my</Button>
     </div>
   );
 }
