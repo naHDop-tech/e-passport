3f4a707ce750a3dc9d914b47277e095cf92dfb8e
Grigory-js-ninja <grigory@maroo.us>
2022-07-24 15:59:02 +0300

    feat: add persist to theme state

--- client/src/store/theme/atoms.ts
+++ client/src/store/theme/atoms.ts
@@ -1,6 +1,10 @@
 import { atom } from 'recoil'
+import { localStorageEffect } from './effect'
 
 export const isDarkMode = atom({
   key: 'themeMode',
-  default: false
+  default: false,
+  effects: [
+    localStorageEffect('theme_mode')
+  ]
 })
