6411383e1bba43eb9017aa9b59f4734b5939dc56
Grigory-js-ninja <grigory@maroo.us>
2022-08-21 18:16:38 +0300

    feat: add color-accent to color theme

--- client/src/store/theme/types.ts
+++ client/src/store/theme/types.ts
@@ -7,11 +7,13 @@ export const themes = {
   light: {
     '--color-background': '#1d2335',
     '--color-text': '#ffffff',
+    '--color-accent': '#f1f1f5'
   },
   dark: {
     '--color-background': '#ffffff',
     '--color-text': '#1d2335',
+    '--color-accent': '#171725'
   },
 }
 
-export type ColorTypes = '--color-background' | '--color-text'
+export type ColorTypes = '--color-background' | '--color-text' | '--color-accent'
