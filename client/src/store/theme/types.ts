3ed65825123fed877321134d59b7f920d08ed59a
Grigory-js-ninja <grigory@maroo.us>
2022-08-23 01:01:29 +0300

    feat: add css var for for background

--- client/src/store/theme/types.ts
+++ client/src/store/theme/types.ts
@@ -7,13 +7,15 @@ export const themes = {
   light: {
     '--color-background': '#1d2335',
     '--color-text': '#ffffff',
-    '--color-accent': '#171725'
+    '--color-accent': '#171725',
+    '--color-form-background': '#171725',
   },
   dark: {
     '--color-background': '#ffffff',
     '--color-text': '#1d2335',
-    '--color-accent': '#f1f1f5'
+    '--color-accent': '#f1f1f5',
+    '--color-form-background': '#ffffff'
   },
 }
 
-export type ColorTypes = '--color-background' | '--color-text' | '--color-accent'
+export type ColorTypes = '--color-background' | '--color-text' | '--color-accent' | '--color-form-background'
