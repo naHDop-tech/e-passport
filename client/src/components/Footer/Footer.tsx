524eb2b28c51d3723271333a791a32e09bd5e697
Grigory-js-ninja <grigory@maroo.us>
2022-08-28 12:39:39 +0300

    feat: add icons to footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -5,8 +5,17 @@ import { Item } from '@store/header/types'
 import { MenuItem } from '@components/Header/components/MenuItem'
 import { headerItemsStateSelector } from '@store/header/selector'
 
+import cs from '@components/CommonStyle.module.css'
+import { ICommonStyle } from '@components/common-style-types'
+
+const commonStyle = cs as ICommonStyle
+
 import s from './FooterStyle.module.css'
 const styles = s as IFooterStyle
+
+import LocationIcon from '@static/icons/location-icon.svg'
+import CallIcon from '@static/icons/call-icon.svg'
+import EmailIcon from '@static/icons/email-icon.svg'
 
 interface IFooterStyle {
   Box: string
@@ -39,9 +48,24 @@ export function Footer(props: IFooterProps) {
 
   return (
     <div className={styles.Box}>
-      <div className={styles.ContactBlock}>ContactBlock</div>
-      <div className={styles.SiteMapBlock}>{menuList}</div>
-      <div className={styles.SocialMediaBlock}>SocialMediaBlock</div>
+      <div className={styles.ContactBlock}>
+        <h3>Di passport</h3>
+        <p className={commonStyle.TextWhiteGrey}>We give you hi end product and secure access</p>
+        <div className={commonStyle.Margin12} />
+        <div><EmailIcon />di-passport@gmail.com</div>
+        <div><CallIcon />+1 (541) 523 34 65</div>
+        <div><LocationIcon />3543 Elgin St. Celina, Delaware 1299</div>
+      </div>
+      <div className={styles.SiteMapBlock}>
+        <h3>Sitemap</h3>
+        <div className={commonStyle.Margin12} />
+        {menuList}
+      </div>
+      <div className={styles.SocialMediaBlock}>
+        <h3>Social Media</h3>
+        <div className={commonStyle.Margin12} />
+        <span>In</span><span>Twi</span><span>Fac</span>
+      </div>
     </div>
   )
 }
