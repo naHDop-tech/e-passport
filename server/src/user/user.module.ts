86e8bda690e81d054b6ce4470fcce42402c89ad7
Grigory-js-ninja <grigory@maroo.us>
2022-08-28 16:58:01 +0300

    feat: delete unused import

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -4,8 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserResolver } from '~/user/user.resolver';
 import { UserService } from '~/user/user.service';
 import { UtilsModule } from '~/utils/utils.module';
-import { ApplicantService } from '~/applicant/applicant.service';
-import { ApplicantEntity } from '~/applicant/applicant.entity';
 import { ApplicantModule } from '~/applicant/applicant.module';
 import { UserEntity } from '~/user/user.entity';
 import { UserFactory } from '~/user/user.factory';
