d6f267dfc2e8861324b729c70316ed1e2c4cac5e
Grigory <90871960+grigory-js-ninja@users.noreply.github.com>
2022-09-06 22:52:21 +0300

    feat: implement Customer info component (#18)
    * feat: add Avatar component
    
    * feat: add state to userInfo component
    
    * style: text ellipsis
    
    * chore: wip
    
    * style: add mediaq
    
    * styles: padding to avatar left
    
    * feat: add fake avatar component

---
+++ client/src/components/Header/components/CustomerInfo/Avatar/Avatar.tsx
@@ -0,0 +1,58 @@
+import { HtmlHTMLAttributes } from 'react'
+import cn from 'classnames'
+
+import { FakeAvatar } from './FakeAvatar'
+import s from './AvatarStyle.module.css'
+const styles = s as IAvatarStyles
+
+
+export interface IAvatarStyles {
+  Box: string
+
+  Small: string
+  Medium: string
+  Large: string
+
+  PrimaryBg: string
+
+  FakeAvatarBox: string
+}
+
+export enum AvatarSizeType {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+}
+
+interface IAvatarProps {
+  imgSrc: string
+  size?: AvatarSizeType
+  isSrcAllowed?: boolean
+}
+
+export const getAvatarSize = (size: AvatarSizeType): string => {
+  if (size === AvatarSizeType.Large) {
+    return styles.Large
+  }
+  if (size === AvatarSizeType.Medium) {
+    return styles.Medium
+  }
+
+  return styles.Small
+}
+
+export type AvatarProps = IAvatarProps & HtmlHTMLAttributes<HTMLDivElement>
+
+
+export function Avatar(props: AvatarProps) {
+  const { imgSrc, size = AvatarSizeType.Small, isSrcAllowed, className, ...rest } = props
+
+  return (
+    <div className={cn(styles.Box, getAvatarSize(size), className)} {...rest}>
+      {isSrcAllowed
+        ? <img className={styles.Box} alt='Customer image' src={imgSrc} /> 
+        : <FakeAvatar size={size} />
+      }
+    </div>
+  )
+}
--- client/src/components/Header/components/CustomerInfo/CustomerInfo.tsx
+++ client/src/components/Header/components/CustomerInfo/CustomerInfo.tsx
@@ -1,11 +1,30 @@
+import { Avatar } from './Avatar'
+
 import s from './CustomerInfoStyle.module.css'
 const styles = s as unknown as ICustomerHeaderStyle
 
 interface ICustomerHeaderStyle {
   Box: string
+
+  EmailBox: string
+  AvatarBox: string
+  FullNameBox: string
 }
-export function CustomerInfo() {
+
+interface ICustomerInfoProps {
+  imgSrc: string
+  email: string
+  customerFullName: string
+}
+
+export function CustomerInfo(props: ICustomerInfoProps) {
+  const { email, customerFullName, imgSrc } = props
+
   return (
-    <div className={styles.Box}></div>
+    <div className={styles.Box}>
+      <Avatar className={styles.AvatarBox} imgSrc={imgSrc} isSrcAllowed={!!imgSrc} />
+      <div className={styles.FullNameBox}>{customerFullName}</div>
+      <div className={styles.EmailBox}>{email}</div>
+    </div>
   )
 }
