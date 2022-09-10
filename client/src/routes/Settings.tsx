1e9470af00ac924e43c57a1b0d2e16908d9e68a8
Grigory-js-ninja <grigory@maroo.us>
2022-09-11 01:03:04 +0300

    feat: implement settings page banner

--- client/src/routes/Settings.tsx
+++ client/src/routes/Settings.tsx
@@ -2,12 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 
 import { GuardRoute } from '@components/GuardRoute'
 import { NotFoundPage } from '@pages/NotFoundPage'
+import { SettingsPage } from '@pages/SettingsPage'
 
 export function SettingsRouter() {
   return (
     <Routes>
       {/* guard routes */}
-      <Route path='/' element={<GuardRoute><p>Settings</p></GuardRoute>} />
+      <Route path='/' element={<GuardRoute><SettingsPage /></GuardRoute>} />
       <Route path='/profile' element={<GuardRoute><p>profile</p></GuardRoute>} />
       <Route path='/preferences' element={<GuardRoute><p>preferences</p></GuardRoute>} />
       <Route path='/notification' element={<GuardRoute><p>notification</p></GuardRoute>} />
