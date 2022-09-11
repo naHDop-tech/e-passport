9a25153779f20f09b0dd79d24f4f176656202fe6
Grigory-js-ninja <grigory@maroo.us>
2022-09-11 11:03:00 +0300

    feat: add dashboard page

--- client/src/routes/Dashboard.tsx
+++ client/src/routes/Dashboard.tsx
@@ -2,13 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 
 import { GuardRoute } from '@components/GuardRoute'
 import { NotFoundPage } from '@pages/NotFoundPage'
+import { Dashboard } from '@pages/Dashboard'
 import { SettingsRouter } from './Settings'
 
 export function DashboardRouter() {
   return (
     <Routes>
       {/* guard routes */}
-      <Route path='/' element={<GuardRoute><p>dashboard</p></GuardRoute>} />
+      <Route path='/' element={<GuardRoute><Dashboard /></GuardRoute>} />
       <Route path='/settings/*' element={<SettingsRouter />} />
       <Route path='/passport' element={<GuardRoute><p>passport</p></GuardRoute>} />
 
