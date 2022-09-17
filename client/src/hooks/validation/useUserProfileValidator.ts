4a776cf26e9826c0ebae19b04ba8010f66fcdb76
Grigory-js-ninja <grigory@maroo.us>
2022-09-18 00:15:30 +0300

    feat: add 21 years old to validator

--- client/src/hooks/validation/useUserProfileValidator.ts
+++ client/src/hooks/validation/useUserProfileValidator.ts
@@ -7,10 +7,13 @@ import { IUserProfile } from '@root/interfaces/user'
 type ValidatorReturnType = Partial<Record<FormFiledIds, string>>
 
 export function useUserProfileValidator(form: Partial<IUserProfile>): ValidatorReturnType {
+  const now = Date.now();
+  const cutoffDate = new Date(now - (1000 * 60 * 60 * 24 * 365 * 21)); // 21 years old
+
   const schema = Joi.object({
     [FormFiledIds.FirstName]: Joi.string().min(3).max(20),
     [FormFiledIds.LastName]: Joi.string().min(3).max(30),
-    [FormFiledIds.BirthDate]: Joi.date().iso(),
+    [FormFiledIds.BirthDate]: Joi.date().iso().max(cutoffDate),
     [FormFiledIds.CountryResident]: Joi.string()
   })
 
