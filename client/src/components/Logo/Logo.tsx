dde14cedf8cce6d531a878ce39b21cae58a9b082
Roman Kholopov <roma.kholopov@gmail.com>
2022-09-21 14:49:21 +0300

    refactor: returned the logo to the original state~

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -1,13 +1,12 @@
 import s from './LogoStyle.module.css'
 const styles = s as unknown as ILogoStyle
-import { PropsWithChildren } from 'react';
 
 interface ILogoStyle {
   Box: string
   Paragraph: string
 }
 
-export function Logo(props: PropsWithChildren<ILogoStyle>) {
+export function Logo() {
   
   return (
     <div className={styles.Box}>
