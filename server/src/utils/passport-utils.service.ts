9c20b613ebb4b74f109db765acf59f896f082729
Grigory-js-ninja <grigory@maroo.us>
2022-12-18 22:05:14 +0100

    feat: add checksums numbers

--- server/src/utils/passport-utils.service.ts
+++ server/src/utils/passport-utils.service.ts
@@ -26,6 +26,11 @@ export class PassportUtilsService {
     getMachineReadableZoneLines(payload: IMRZLinesProps): IMRZLines {
         let mrzL1 = ''
         let mrzL2 = ''
+        
+        const check1 = this.randomIntFromInterval(1, 9)
+        const check2 = this.randomIntFromInterval(1, 9)
+        const check3 = this.randomIntFromInterval(1, 9)
+        const checkSum = this.simpleHash(`${check1}${check2}${check3}`)
 
         // Build line 1
         mrzL1 += `${payload.type}<${payload.countryCode}${payload.firstName}<<${payload.lastName}`
@@ -39,19 +44,15 @@ export class PassportUtilsService {
         
         const sexId = payload.sex.charAt(0)
         // Build line 2
-        mrzL2 += `${payload.pNumber}<${7}${payload.nationality}${payload.dateOfBirth}${4}${sexId}${payload.expirationDate}${7}${payload.uNumber}`
+        mrzL2 += `${payload.pNumber}<${check1}${payload.nationality}${payload.dateOfBirth}${check2}${sexId}${payload.expirationDate}${check3}${payload.uNumber}`
 
         if (mrzL1.length >= LINE_LENGTH) {
             throw new Error('Data for line 2 is too long')
         }
         while (mrzL1.length < LINE_LENGTH) {
-            if (mrzL2.length === 42) {
-                mrzL2 += '7'
-                continue
-            }
-            if (mrzL2.length === 43) {
-                mrzL2 += '8'
-                continue
+            if (mrzL2.length === 41) {
+                mrzL2 += checkSum
+                break
             }
             mrzL2 += '<'
         }
@@ -60,5 +61,19 @@ export class PassportUtilsService {
             mrzL1,
             mrzL2,
         }
+    }
+
+    private simpleHash(str: string): string {
+        let hash = 0;
+        for (let i = 0; i < str.length; i++) {
+            const char = str.charCodeAt(i);
+            hash = (hash << 5) - hash + char;
+            hash &= hash; // Convert to 32bit integer
+        }
+        return new Uint32Array([hash])[0].toString(36);
+    };
+
+    private randomIntFromInterval(min, max): number {
+        return Math.floor(Math.random() * (max - min + 1) + min)
     }
 }
